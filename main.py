ac74f1b1e59680fc0fc5fae5b70c3580abbb86df
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-02-06 18:00:46 +0100

    WIP

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ candidate2 = input("Enter the name of the second candidate: ")
 
 candidate1_votes = 0
 candidate2_votes = 0
+
+voter_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number_of_voters = len(voter_id)
+
+while True:
+    if not voter_id:
+        print("Voting is complete")
+        if candidate1_votes > candidate2_votes:
+            percentage = (candidate1_votes / number_of_voters) * 100
+            print(f"{candidate1} has won the election with {percentage}% of votes")
+        elif candidate2_votes > candidate1_votes:
+            percentage = (candidate2_votes / number_of_voters) * 100
+            print(f"{candidate2} has won the election with {percentage}% of votes")
+            break
+        else:
+            print("Equality !")
