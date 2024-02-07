d012f5a45040538aff33a32fc73a29a21643987c
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-02-07 02:59:54 +0100

    FINISHED

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ while True:
         elif candidate2_votes > candidate1_votes:
             percentage = (candidate2_votes / number_of_voters) * 100
             print(f"{candidate2} has won the election with {percentage}% of votes")
-            break
         else:
             print("Equality !")
+        break
+
+    voter = int(input("Enter your voter ID: "))
+    if voter in voter_id:
+        print("You can vote")
+        voter_id.remove(voter)
+        print("To vote for candidate1, press 1")
+        print("To vote for candidate2, press 2")
+
+        vote = int(input("Enter your vote: "))
+        if vote == 1:
+            candidate1_votes += 1
+            print("Thank you for voting candidate 1.")
+        elif vote == 2:
+            candidate2_votes += 1
+            print("Thank you for voting candidate 2.")
+        else:
+            print("Invalid vote. Please try again with 1 or 2.")
+    else:
+        print("You have already voted or your voter ID is invalid. Please try again.")
